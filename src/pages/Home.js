ca3c168961413ec0493b7fb1193aa2d2291cbd57
Mangocod3r <117846037+Mangocod3r@users.noreply.github.com>
2023-04-10 18:44:20 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,11 +45,87 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 // import { Helmet } from 'react-helmet'
-import { useState } from 'react'
+// import { useState } from 'react'
 import Card from '../components/card'
 import './home.css'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+
+import { useState, useEffect, useRef } from 'react';
+
+// import { useState, useEffect } from 'react';
+
+// function TypingText(props) {
+//   const [index, setIndex] = useState(0);
+//   const [isDeleting, setIsDeleting] = useState(false);
+
+//   useEffect(() => {
+//     const words = props.words;
+//     const current = index % words.length;
+//     const currentWord = words[current];
+
+//     if (isDeleting) {
+//       props.setText(currentWord.substring(0, index - current * currentWord.length));
+//     } else {
+//       props.setText(currentWord.substring(0, index - current * currentWord.length + 1));
+//     }
+
+//     const typingSpeed = props.typingSpeed || 150;
+
+//     const timer = setTimeout(() => {
+//       if (!isDeleting && index >= currentWord.length * (current + 1)) {
+//         setIsDeleting(true);
+//       } else if (isDeleting && index <= currentWord.length * current) {
+//         setIsDeleting(false);
+//         setIndex(prevIndex => prevIndex + 1);
+//       }
+
+//       setIndex(prevIndex => prevIndex + (isDeleting ? -1 : 1));
+//     }, typingSpeed);
+
+//     return () => clearTimeout(timer);
+//   }, [index, isDeleting, props]);
+
+//   return null;
+// }
+
+import Typed from 'typed.js';
+
+const TypingText = (props) => {
+  const elRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: props.words,
+      typeSpeed: props.typingSpeed || 50,
+      backSpeed: props.backSpeed || 30,
+      startDelay: props.startDelay || 0,
+      backDelay: props.backDelay || 500,
+      loop: props.loop || false,
+      loopCount: props.loopCount || Infinity,
+      showCursor: props.showCursor || true,
+      cursorChar: props.cursorChar || '|',
+      contentType: props.contentType || 'html',
+      onComplete: (self) => {
+        props.onComplete && props.onComplete(self);
+      }
+    };
+
+    const typed = new Typed(elRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, [props]);
+
+  return <span className='home-text10' ref={elRef} />;
+}
+
 const Home = (props) => {
+
+  // const [text, setText] = useState('');
+  // // const words = ['Empowering Students and Entrepreneurs to Innovate Together'];
+  // const words = [' Innovate Collaborate Succeed'];
+  // const words = ['Collaborate', 'Succeed', 'Innovate'];
 
   const [questions, setQuestions] = useState([
     {
@@ -89,7 +165,7 @@ const Home = (props) => {
     newQuestions[index].isOpen = !newQuestions[index].isOpen;
     setQuestions(newQuestions);
   };
-  
+
   const renderCard = (card, index) => {
     return (
       <div key={index} className="question">
@@ -107,20 +183,20 @@ const Home = (props) => {
       </div>
     );
   };
-  
+
 
   return (
-    <motion.div className="home-container" style={{marginTop:'-20px'}}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 0.5 }}
-          >
+    <motion.div className="home-container" style={{ marginTop: '-20px' }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 0.5 }}
+    >
       {/* <Helmet> */}
-        <title>Up Start Template</title>
-        <meta property="og:title" content="Up Start Template" />
+      <title>Up Start Template</title>
+      <meta property="og:title" content="Up Start Template" />
       {/* </Helmet> */}
       {/* <div className="navbar-container"> */}
-        {/* <div className="max-width">
+      {/* <div className="max-width">
           <div className="home-logo">
             <img
               alt="image"
@@ -150,20 +226,28 @@ const Home = (props) => {
       <div className="hero-container section-container">
         <div className="home-max-width1 max-width">
           <div className="home-content">
-            {/* <span className="home-subtitle beforeHeading">AKOVA PORTAL</span> */}
             <h1 className="home-title">
               <span>Empowering Students and Entrepreneurs to  </span>
               <br></br>
-              {/* <span> </span> */}
-              <span className="home-text10">Innovate</span>
+              {/* <TypingText words={words} setText={setText} /> */}
+              {/* <span className='home-text10'>{text}</span> */}
+              <TypingText
+                words={['Collaborate', 'Innovate', 'Succeed']}
+                typingSpeed={50}
+                startDelay={1000}
+                backDelay={1000}
+                loop={true}
+                loopCount={Infinity}
+              />
               <span> Together</span>
+
             </h1>
             <span className="home-description">
-            Join our community of aspiring entrepreneurs and students to share and explore ideas, showcase your skills, and collaborate on groundbreaking projects that have the potential to change the world
+              Join our community of aspiring entrepreneurs and students to share and explore ideas, showcase your skills, and collaborate on groundbreaking projects that have the potential to change the world
             </span>
             <div className="home-container01">
-            <Link style={{textDecoration:'none'}}to="/login" className="butto-out">Join for free</Link>
-              <Link style={{textDecoration:'none'}}to="/login" className="butto-in">Log in</Link>
+              <Link style={{ textDecoration: 'none' }} to="/login" className="butto-out">Join for free</Link>
+              <Link style={{ textDecoration: 'none' }} to="/login" className="butto-in">Log in</Link>
             </div>
           </div>
           <div className="home-image1">
@@ -189,26 +273,26 @@ const Home = (props) => {
       <div className="section-container">
         <div className="home-max-width2 max-width">
           <div className="home-image3">
-          <img
-            alt="image"
-            src="https://media.istockphoto.com/id/1287030300/photo/drop-shipping-business-owner-confirming-the-order-on-phone.jpg?s=612x612&w=0&k=20&c=XfaGtdTFUZofCWdOC70kiJ_DHls-a9VIdAwBdctBvoQ="            // src="/playground_assets/istockphoto-1301397300-612x612-1200w.jpg"
-            className="home-hero-image1"
-          />
+            <img
+              alt="image"
+              src="https://media.istockphoto.com/id/1287030300/photo/drop-shipping-business-owner-confirming-the-order-on-phone.jpg?s=612x612&w=0&k=20&c=XfaGtdTFUZofCWdOC70kiJ_DHls-a9VIdAwBdctBvoQ="            // src="/playground_assets/istockphoto-1301397300-612x612-1200w.jpg"
+              className="home-hero-image1"
+            />
           </div>
-          
+
           <motion.div className="home-content1"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5, duration: 1.5 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5, duration: 1.5 }}
           >
             <span className="home-text11 beforeHeading">ENTREPRENUER</span>
             <h1 className="home-text12">
-              
+
               {/* <span>Heading 1</span> */}
               {/* <br></br> */}
             </h1>
             <span className="home-text15">
-            Are you looking for the next big thing? Our platform offers you a unique opportunity to discover innovative projects submitted by talented students. Upload your projects, connect with budding entrepreneurs and potentially invest in the next game-changing idea. Take the first step towards creating a successful future, today
+              Are you looking for the next big thing? Our platform offers you a unique opportunity to discover innovative projects submitted by talented students. Upload your projects, connect with budding entrepreneurs and potentially invest in the next game-changing idea. Take the first step towards creating a successful future, today
             </span>
             <div className="home-container02">
               <button className="my-button  bb blue">
@@ -216,7 +300,7 @@ const Home = (props) => {
               </button>
             </div>
           </motion.div>
-          
+
         </div>
       </div>
       <motion.div className="home-section1 section-container"
@@ -233,7 +317,7 @@ const Home = (props) => {
               {/* <span className="home-text20">Heading 2</span> */}
             </h1>
             <span className="home-text21">
-            Unlock your potential and take the first step towards your entrepreneurial journey! Join our platform designed exclusively for students, where you can explore innovative projects, submit your ideas, and learn from experienced entrepreneurs. Collaborate with like-minded individuals, gain valuable insights and transform your ideas into reality!
+              Unlock your potential and take the first step towards your entrepreneurial journey! Join our platform designed exclusively for students, where you can explore innovative projects, submit your ideas, and learn from experienced entrepreneurs. Collaborate with like-minded individuals, gain valuable insights and transform your ideas into reality!
             </span>
             <div className="home-container03">
               <button className="my-button bb blue">
@@ -270,7 +354,7 @@ const Home = (props) => {
               <div className="home-container04">
                 <span className="home-title1">Create your free account</span>
                 <span className="home-text25">
-                Step into the future of innovation with us by creating your free account today
+                  Step into the future of innovation with us by creating your free account today
                 </span>
               </div>
             </div>
@@ -283,7 +367,7 @@ const Home = (props) => {
                   Learn  &amp; upload by your field
                 </span>
                 <span className="home-text27">
-                Learn, upload, and collaborate with like-minded individuals in your field to turn your ideas into reality.
+                  Learn, upload, and collaborate with like-minded individuals in your field to turn your ideas into reality.
 
                 </span>
               </div>
@@ -295,7 +379,7 @@ const Home = (props) => {
               <div className="home-container06">
                 <span className="home-title3">Fund ideas </span>
                 <span className="home-text29">
-                Join the movement of funding groundbreaking projects that have the potential to change the world
+                  Join the movement of funding groundbreaking projects that have the potential to change the world
                 </span>
               </div>
             </div>
@@ -310,7 +394,7 @@ const Home = (props) => {
             <br></br> */}
             <span>Take your skills to new heights with Akova's innovation community</span>
           </h1>
-          
+
           <div className="home-cards-container">
             <Card rootClassName="card-root-class-name"></Card>
             <Card text="Personal" rootClassName="card-root-class-name2"></Card>
@@ -404,7 +488,7 @@ const Home = (props) => {
             </h1>
             <span className="home-text53">
               <span>
-              Unleash your potential and turn your ideas into reality with Akova  <span
+                Unleash your potential and turn your ideas into reality with Akova  <span
                   dangerouslySetInnerHTML={{
                     __html: ' ',
                   }}
@@ -412,7 +496,7 @@ const Home = (props) => {
               </span>
               <br></br>
               <span>
-              the ultimate innovation hub for students and entrepreneurs                               <span
+                the ultimate innovation hub for students and entrepreneurs                               <span
                   dangerouslySetInnerHTML={{
                     __html: ' ',
                   }}
