1341abcc22ac3a68a2fa7356a5fa65092aee8a3e
Mangocod3r <117846037+Mangocod3r@users.noreply.github.com>
2023-06-21 15:24:13 +0530

    Update BasicExample.js

--- src/components/BasicExample.js
+++ src/components/BasicExample.js
@@ -5,12 +5,24 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+// import {ColorModeContext} from "../pages/ColorModeContext";
+// import { ColorModeContext } from "./pages/ColorModeContext";
 import './Navbar.css';
 
-export default function BasicExample() {
+export default function BasicExample({darkMode}) {
   const { logout } = useLogout()
   const { user } = useAuthContext()
+ 
+  darkMode ? console.log("hi") : console.log("hio"); // Check if "darkMode" prop is being passed or not
+  // const { colorMode } = useContext(ColorModeContext);
+  // console.log(colorMode);
+
+  // const bgColor = colorMode === "light" ? "#FFFFFF" : "#111111";
+  // const textColor = colorMode === "light" ? "#000000" : "#FFFFFF";
+  // const akovaColor = colorMode === "light" ? "#0056D2" : "#FFFFFF";
+  // const backgroundColor = colorMode === "light" ? "white" : "black";
+  // const textColor = colorMode === "light" ? "#0056D2" : "white";
 
   const [cats, setCats] = useState([
     {
@@ -69,7 +81,7 @@ export default function BasicExample() {
     return (
       <Navbar bg="light" expand="lg" className='shadow p-2  bg-white rounded sticky-top'>
         <Container>
-          <Navbar.Brand style={{ textDecoration: 'none', color: '#0056d2', fontWeight: '600', fontSize: '30px', marginTop: '15px' }} href="/" >AKOVA</Navbar.Brand>
+          <Navbar.Brand style={{ textDecoration: 'none', color: "0056D2", fontWeight: '600', fontSize: '30px', marginTop: '15px' }} href="/" >AKOVA</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" >
             {/* <i className={this.state.clicked ? "fas-fa-times" : "fas fa-bars"}>
             </i> */}
@@ -112,6 +124,44 @@ export default function BasicExample() {
     );
   }
 
+  else if (user.role === 'Investor') {
+    return (
+      <Navbar bg="light" expand="lg" className='shadow p-2  bg-white rounded sticky-top'>
+        <Container>
+          <Navbar.Brand style={{ textDecoration: 'none', color: "0056D2", fontWeight: '600', fontSize: '30px', marginTop: '15px' }} href="/" >AKOVA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" >
+            {/* <i className={this.state.clicked ? "fas-fa-times" : "fas fa-bars"}>
+            </i> */}
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="s2">
+              <Nav.Link href="/" style={{ textDecoration: 'none' }} className='navi'>Home</Nav.Link>
+              <Nav.Link href="/inv_req" style={{ textDecoration: 'none' }}>Requests </Nav.Link>
+              <Nav.Link href="/inv_sch" style={{ textDecoration: 'none' }}>Schedule</Nav.Link>
+              <Nav.Link href="/inv_men" style={{ textDecoration: 'none' }}>Mentoring</Nav.Link>
+              <Nav.Link href="/inv_met" style={{ textDecoration: 'none' }}>Metrics</Nav.Link>
+              {user && (
+                <div className="s1">
+                  {/* <div className="s1-text hide-on-small-screens"> */}
+                  <span className='intro' style={{ color: '#0023A2', display: 'inline-block' }}><em>Welcome back</em> </span><span className='intro2' style={{ marginLeft: '10px' }} >{user.name}</span>
+                  {/* </div> */}
+                  <button className='my-button butto-in1' onClick={handleClick}>Log out</button>
+                </div>
+              )}
+              {!user && (
+                <div>
+                  <Link to="/login">Login</Link>
+                  <Link to="/signup">Signup</Link>
+                </div>
+              )}
+            </Nav>
+
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+
   else if (user.role === 'Entreprenuer') {
     return (
       <Navbar bg="light" expand="lg" className='shadow p-2  bg-white rounded sticky-top' style={{ marginBottom: '-20px' }}>
@@ -123,6 +173,7 @@ export default function BasicExample() {
               <Nav.Link style={{ textDecoration: 'none' }} href="/">Home</Nav.Link>
               {/* <Nav.Link href="/">My Projects</Nav.Link> */}
               <Nav.Link style={{ textDecoration: 'none' }} href="/upload">Upload a problem</Nav.Link>
+              <Nav.Link style={{ textDecoration: 'none' }} href="/ent_prop">Propose a investment</Nav.Link>
             </Nav>
 
             {user && (
@@ -146,6 +197,42 @@ export default function BasicExample() {
       </Navbar>
     );
   }
+
+  // else{
+  //   return (
+  //     <Navbar bg="light" expand="lg" className='shadow p-2  bg-white rounded sticky-top' style={{ marginBottom: '-20px' }}>
+  //       <Container>
+  //         <Navbar.Brand style={{ textDecoration: 'none', color: '#0056d2', fontWeight: '600', fontSize: '30px', marginTop: '15px' }} href="/">AKOVA</Navbar.Brand>
+  //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //         <Navbar.Collapse id="basic-navbar-nav">
+  //           <Nav className=" justify-content-center p-2">
+  //             <Nav.Link style={{ textDecoration: 'none' }} href="/">Home</Nav.Link>
+  //             {/* <Nav.Link href="/">My Projects</Nav.Link> */}
+  //             <Nav.Link style={{ textDecoration: 'none' }} href="/upload">Upload a problem</Nav.Link>
+  //             <Nav.Link style={{ textDecoration: 'none' }} href="/ent_prop">Propose a investment</Nav.Link>
+  //           </Nav>
+
+  //           {user && (
+  //             <div className="s1">
+  //               {/* <div className="s1-text hide-on-small-screens"> */}
+  //               <span className='intro' style={{ color: '#0023A2', display: 'inline-block' }}><em>Welcome back</em> </span><span className='intro2' style={{ marginLeft: '10px' }} >{user.name}</span>
+  //               {/* </div> */}
+  //               <button className='my-button butto-in1' onClick={handleClick}>Log out</button>
+  //             </div>
+  //           )}
+
+
+  //           {!user && (
+  //             <div>
+  //               <Link to="/login">Login</Link>
+  //               <Link to="/signup">Signup</Link>
+  //             </div>
+  //           )}
+  //         </Navbar.Collapse>
+  //       </Container>
+  //     </Navbar>
+  //   );
+  // }
 
 }
 // export default BasicExample;
