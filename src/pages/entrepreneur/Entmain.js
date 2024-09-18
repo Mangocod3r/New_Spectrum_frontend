cd986714797f5790acbfec73eeea9637f6475636
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2024-09-18 09:29:16 +0530

    Update Entmain.js

--- src/pages/entrepreneur/Entmain.js
+++ src/pages/entrepreneur/Entmain.js
@@ -3,49 +3,137 @@ import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/useAuthContext'
 import fetchWithAuth from "../../api/fetchWithAuth";
 
+// export default function Entmain() {
+
+//   const { user } = useAuthContext()
+
+// console.log(user)
+//   const [cats, setCats] = useState([
+//     {
+//       header:"",
+//       overview: "",
+//       title: "",
+//       img: "",
+//       p1: "",
+//       p2: "",
+//       p3: "",
+//       sub: "",
+//       text: " ",
+//       description: "",
+//       start: "",
+//       end: "",
+//     }
+//   ]);
+
+//   useEffect(() => {
+//     fetchWithAuth(`${process.env.REACT_APP_API_HOST}/posts`)
+
+//       .then((res) => res.json())
+//       .then((jsonRes) => setCats(jsonRes));
+//   }, []);
+
+//   useEffect(() => {
+//     console.log(cats);
+//   }, [cats]);
+
+//   const post = cats.filter(post => post.name === user.name)
+
+//   const renderCard = (cats, index) => {
+//     return (
+//       <tr>
+//         <td>
+//           <h1 className="text-center " style={{color:'#0056D2'}}>{cats.header}</h1>
+//           <div className="shadow p-3 mb-5 bg-red rounded main1">
+//             <div>
+//               {}
+//               <p className="overview_text">
+//                 {cats.overview}
+//               </p>
+//               <div className="display-5 text-center entbut">
+//                 <Link to={`/entresp/${cats.header}`}>
+//                   <button className="my-button bb blue s overview_button">
+//                     SEE YOUR RESPONSES
+//                   </button>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         </td>
+//       </tr>
+//     );
+//   };
+
+//   if (post.length === 0) {
+//     return (
+//       <p className="no"><em>No existing projects yet </em>
+//       <div>
+//         <span>Upload 
+//         <Link to='/upload'>
+//           <span style={{textDecoration:'none'}}className='link'><bold> HERE </bold></span>
+//           </Link>
+//         to start your journey</span>
+//         </div>
+//         </p>
+//     );
+//   }
+//   else{
+//   return (
+//     <>
+//       <div className="container-fluid main mt-5"  id="productTable">
+//         <div class="container d-flex justify-content-end">
+//           <Link to='/ent_accinv'>
+//              Accepted Investments
+//           </Link>
+//         </div>
+//         {}
+//         {}
+//         <table className="table-fill">
+//           <thead>
+//             <tr>
+//               <th>
+//               </th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {post.map(renderCard)}
+//           </tbody>
+//         </table>
+//       </div>
+//       {}
+//     </>
+//   );
+//  }
+// }
 export default function Entmain() {
+  const { user } = useAuthContext();
 
-  const { user } = useAuthContext()
-
-console.log(user)
-  const [cats, setCats] = useState([
-    {
-      header:"",
-      overview: "",
-      title: "",
-      img: "",
-      p1: "",
-      p2: "",
-      p3: "",
-      sub: "",
-      text: " ",
-      description: "",
-      start: "",
-      end: "",
-    }
-  ]);
+  const [cats, setCats] = useState([]);
 
   useEffect(() => {
     fetchWithAuth(`${process.env.REACT_APP_API_HOST}/posts`)
-
       .then((res) => res.json())
-      .then((jsonRes) => setCats(jsonRes));
+      .then((jsonRes) => {
+        if (Array.isArray(jsonRes)) {
+          setCats(jsonRes);
+        } else {
+          setCats([]); // Handle unexpected response format
+        }
+      });
   }, []);
 
   useEffect(() => {
     console.log(cats);
   }, [cats]);
 
-  const post = cats.filter(post => post.name === user.name)
+  const post = Array.isArray(cats) ? cats.filter(post => post.name === user.name) : [];
 
   const renderCard = (cats, index) => {
     return (
-      <tr>
+      <tr key={index}>
         <td>
-          <h1 className="text-center " style={{color:'#0056D2'}}>{cats.header}</h1>
+          <h1 className="text-center " style={{ color: '#0056D2' }}>{cats.header}</h1>
           <div className="shadow p-3 mb-5 bg-red rounded main1">
             <div>
-              {}
               <p className="overview_text">
                 {cats.overview}
               </p>
@@ -66,32 +154,28 @@ console.log(user)
   if (post.length === 0) {
     return (
       <p className="no"><em>No existing projects yet </em>
-      <div>
-        <span>Upload 
-        <Link to='/upload'>
-          <span style={{textDecoration:'none'}}className='link'><bold> HERE </bold></span>
-          </Link>
-        to start your journey</span>
+        <div>
+          <span>Upload
+            <Link to='/upload'>
+              <span style={{ textDecoration: 'none' }} className='link'><b> HERE </b></span>
+            </Link>
+            to start your journey</span>
         </div>
-        </p>
+      </p>
     );
   }
-  else{
   return (
     <>
-      <div className="container-fluid main mt-5"  id="productTable">
-        <div class="container d-flex justify-content-end">
+      <div className="container-fluid main mt-5" id="productTable">
+        <div className="container d-flex justify-content-end">
           <Link to='/ent_accinv'>
-             Accepted Investments
+            Accepted Investments
           </Link>
         </div>
-        {}
-        {}
         <table className="table-fill">
           <thead>
             <tr>
-              <th>
-              </th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -99,8 +183,6 @@ console.log(user)
           </tbody>
         </table>
       </div>
-      {}
     </>
   );
- }
 }
